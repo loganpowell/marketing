3e9323c508a93ab7eaa7062219db28061459e8c1
Logan Powell <loganp@tepper.cmu.edu>
2019-06-19 16:13:52 -0400

    fixes inappropriate jobs-widget page

---
+++ out/_next/static/grV4ZjBJjI4llZo_lUu3R/pages/calendar.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["b6d8"],{g0vX:function(n,w,d){(window.__NEXT_P=window.__NEXT_P||[]).push(["/calendar",function(){var n=d("B+Aq");return{page:n.default||n}}])}},[["g0vX","5d41","9da1"]]]);
